ce17cd67f0a6ae2a756af5caaba22ef02d99e947
Oleksiy Pototskyy <oleksiy@pototskyy.net>
2023-10-12 21:11:32 +0200

    Our new code

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = "1.5.7"
+}
+
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.44.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region         = "eu-central-1"
+    key            = "hetzner-example.tfstate"
+    bucket         = "terraform-in-5-minutes"
+  }
+}
+
+provider "hcloud" {
+  # Configuration options
+  token = "ndueSGkHGdNFfz64CEuSrS6st3whKu1yMAUbDfk3MAufB5IsawBlne8G5Io9UJVB"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "hcloud_ssh_key" "example" {
+  name       = "example"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "hcloud_server" "example" {
+  name        = "example"
+  image       = "ubuntu-22.04"
+  server_type = "cx11"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.example.id]
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+
+  user_data = <<EOD
+#!/bin/sh
+apt -y update
+apt install -y apache2
+EOD
+}
+
